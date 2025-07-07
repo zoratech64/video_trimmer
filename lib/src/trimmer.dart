4f05562e7babeac25b6a07fe6169380fb175024f
ZoRa Tech <155092414+zoratech64@users.noreply.github.com>
2025-07-07 14:35:39 +0430

    Update trimmer.dart

--- lib/src/trimmer.dart
+++ lib/src/trimmer.dart
@@ -3,8 +3,8 @@ import 'dart:io';
 import 'dart:typed_data';
 import 'package:image/image.dart' as img;
 import 'package:flutter_native_video_trimmer/flutter_native_video_trimmer.dart';
-import 'package:get_thumbnail_video/index.dart';
-import 'package:get_thumbnail_video/video_thumbnail.dart';
+import 'package:get_video_thumbnail/index.dart';
+import 'package:get_video_thumbnail/get_video_thumbnail.dart';
 import 'package:path/path.dart';
 
 import 'package:flutter/material.dart';
